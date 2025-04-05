f54c1c6baeffeb854984fde2e67a10cb17972c08
Teilanialicia <126744697+Teilanialicia@users.noreply.github.com>
2025-04-05 13:28:20 -0700

    week05 assignment

---
+++ week05/Mindfulness/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace week05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int menuSelection = 0;
+
+            while (menuSelection != 4)
+            {
+                DisplayMenu();
+                menuSelection = GetMenuSelection();
+
+                if (menuSelection == 1)
+                {
+                    BreathingActivity breathingActivity = new BreathingActivity("Breathing", "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.");
+                    breathingActivity.Run();
+                }
+                else if (menuSelection == 2)
+                {
+                    ReflectingActivity reflectingActivity =  new ReflectingActivity("Reflecting", "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.");
+                    reflectingActivity.Run();
+                }
+                else if (menuSelection == 3)
+                {
+                    ListingActivity listingActivity = new ListingActivity(5, "Listing", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
+                    listingActivity.Run();
+                }
+            }
+        }
+
+        static void DisplayMenu()
+        {
+            Console.Clear();
+            Console.Write("Menu Options:\n  1. Start breathing activity\n  2. Start reflecting activity\n  3. Start listing activity\n  4. Quit\nSelect a choice from the menu: ");
+        }
+
+        static int GetMenuSelection()
+        {
+            try
+            {
+                int menuSelection = int.Parse(Console.ReadLine());
+
+                if (menuSelection > 4 || menuSelection < 1)
+                    throw new Exception();
+                
+                return menuSelection;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Please enter a valid menu selection (1-4)");
+                // Sleep for 3 seconds so the user sees the error before it is cleared by DisplayMenu
+                Thread.Sleep(2000);
+                return 0;
+            }
+        }
+    }
+}
---
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -0,0 +1,76 @@
+using System.Reflection.Metadata;
+using System.Text.Json;
+
+namespace week05
+{
+    class ReflectingActivity: Activity
+    {
+        public List<string> _prompts { get; set; }
+        public List<string> _questions { get; set; }
+
+        public ReflectingActivity(string name, string description): base(name, description)
+        {
+            _prompts = ReadListFromFile("reflectingPrompts.txt");
+            _questions = ReadListFromFile("reflectingQuestions.txt");
+        }
+
+        // This is part of showing my creativity - I created this method to read in all of the lists from another file so the prompts and questions can easily be updated
+        private List<string> ReadListFromFile(string fileName)
+        {
+            string text = File.ReadAllText($"{Directory.GetCurrentDirectory()}\\{fileName}");
+            return new List<string>(text.Split(","));
+        }
+
+        public void Run()
+        {
+            DisplayStartingMessage();
+
+            Console.WriteLine($"Consider the following prompt:\n\n --- {GetRandomPrompt()} ---\n\nWhen you have something in mind, press enter to continue.");
+            Console.ReadLine();
+            ShowBeginCountDown(3);
+            DisplayQuestions();
+            DisplayEndingMessage();
+        }
+
+        private string GetRandomPrompt()
+        {
+            Random random = new Random();
+            return _prompts[random.Next(_prompts.Count)];
+        }
+
+        private string GetRandomQuestion()
+        {
+            Random random = new Random();
+            return _questions[random.Next(_questions.Count)]; 
+        }
+
+        public void DisplayPrompt()
+        {
+            Console.WriteLine(GetRandomPrompt());
+        }
+
+        public void DisplayQuestions()
+        {
+            Console.Clear();
+
+            int runtime = _duration;
+            // The time that a question is shown in seconds
+            int questionDuration = 10;
+
+            // If the runtime is less than the defined 10 seconds for the questions, just make the question only last for runtime amount of seconds
+            if (runtime < 10)
+            {
+                questionDuration = runtime;
+            }
+
+            while (runtime > 0)
+            {
+                Console.Write(GetRandomQuestion());
+                ShowSpinner(questionDuration);
+
+                runtime = runtime - questionDuration;
+                Console.WriteLine("\n\n");
+            }
+        }
+    }
+}
